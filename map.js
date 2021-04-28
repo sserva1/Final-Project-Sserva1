36c4348711bafe00f16cd1e1e9045b52599a22f8
sserva1 <77353742+sserva1@users.noreply.github.com>
2021-04-27 20:56:57 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,14 @@
-
+function init () {
+  let demoMap = L.map('map').setView([32.18, -99.14], 4)
+  L.tileLayer('https://sserva1.github.io/Final-project/', {
+  }).addTo(demoMap)
+  let city = L.marker([30, -90]).addTo(demoMap)
+  let area = L.polygon([
+    [31, -88],
+    [35, -88],
+    [32, -82]
+  ]).addTo(demoMap)
+  area.bindPopup('A polygon')
+  city.bindPopup('A marker')
+}
+window.addEventListener('load', init)
